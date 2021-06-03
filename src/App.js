64413e158bb1a56dbd317fdcc1a67a425f6abf51
anshumanxda <anshumanpathak4@gmail.com>
2021-06-03 21:49:53 +0530

    added nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import GlobalStyles from "./components/GlobalStyles";
 import { Route } from "react-router-dom";
+
 function App() {
   return (
     <div>
       <GlobalStyles />
+      <Nav />
       <Route path={["/game/:id", "/"]}>
         <Home />
       </Route>
